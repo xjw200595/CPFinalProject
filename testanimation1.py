9e24f9207cb0c8c17853e9be496331d5e8dcfda2
xjw200595 <jx750@nyu.edu>
2016-12-11 23:45:40 -0500

    cannot export

--- testanimation1.py
+++ testanimation1.py
@@ -1,5 +1,5 @@
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from matplotlib import animation
 
 def trans(x,y,z):
@@ -11,30 +11,42 @@ theta = np.load("theta.npy")
 
 phi = np.load("phi.npy")
 
-# First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure()
-ax = plt.axes(xlim=(-2, 2), ylim=(-2, 2)).set_aspect('equal')
+fig, ((ax)) = plt.subplots(nrows=1, ncols=1)
+ax.set_xlim(-2,2)
+ax.set_ylim(-2,2)
+ax.set_aspect('equal')
 
-line1, = ax.plot([], [], '--', lw=2)
-'''
-line2,= ax.plot([], [], '--', lw=2)
-line3,= ax.plot([], [], '--', lw=2)
-line4,= ax.plot([], [], '.', lw=2)
-line5,= ax.plot([], [], lw=2)
-line6,= ax.plot([], [], '.', lw=2)
-line7,= ax.plot([], [], '.', lw=2)
 
- #initialization function: plot the background of each frame
+line1, = ax.plot([], [], 'b--', lw=2)
+line2,= ax.plot([], [], 'b--', lw=2)
+line3,= ax.plot([], [], 'b--', lw=2)
+line4,= ax.plot([], [], 'r.', lw=2)
+line5,= ax.plot([], [], 'k', lw=1)
+line6,= ax.plot([], [], 'k', lw=1)
+line62,= ax.plot([], [], 'k--', lw=1)
+line7,= ax.plot([], [], 'k', lw=1)
+line72,= ax.plot([], [], 'k--', lw=1)
+line8,= ax.plot([], [], 'g', lw=1)
+line82,= ax.plot([], [], 'g--', lw=1)
+
 def init():
     line1.set_data([], [])
     line2.set_data([], [])
     line3.set_data([], [])
     line4.set_data([], [])
-    return line1, line2, line3, line4, line5, line6, line7
+    line5.set_data([], [])
+    line6.set_data([], [])
+    line7.set_data([], [])
+    line8.set_data([], [])
+    line62.set_data([], [])
+    line72.set_data([], [])
+    line82.set_data([], [])
+    return line1, line2, line3, line4, line5, line6, line7, line62, line72, line8, line82
 
-# animation function.  This is called sequentially
 def animate(i):
     angle = np.linspace(0, 2*np.pi, 1000)
+    angle1 = np.linspace(-0.5*np.pi, 0.5*np.pi, 1000)
+    angle2 = np.linspace(0.5*np.pi, 1.5*np.pi, 1000)
     l1 = np.linspace(0, 0, 1000)
     l2 = np.linspace(-1.5, 1.5, 1000)
     line1.set_data(l1, l2)
@@ -47,21 +59,14 @@ def animate(i):
     line4.set_data(X, Y)
     #print(i)
     line5.set_data(np.cos(angle), np.sin(angle))
-    line6.set_data(trans(np.cos(angle), np.sin(angle), 0)[0], trans(np.cos(angle), np.sin(angle), 0)[1])
-    line7.set_data(trans(np.cos(angle), 0, np.sin(angle))[0], trans(np.cos(angle), 0, np.sin(angle))[1])
-    
-    return line1, line2, line3, line4, line5, line6, line7
+    line6.set_data(trans(np.cos(angle1), np.sin(angle1), 0)[0], trans(np.cos(angle1), np.sin(angle1), 0)[1])
+    line62.set_data(trans(np.cos(angle2), np.sin(angle2), 0)[0], trans(np.cos(angle2), np.sin(angle2), 0)[1])
+    line7.set_data(trans(np.cos(angle1), 0, np.sin(angle1))[0], trans(np.cos(angle1), 0, np.sin(angle1))[1])
+    line72.set_data(trans(np.cos(angle2), 0, np.sin(angle2))[0], trans(np.cos(angle2), 0, np.sin(angle2))[1])
+    line8.set_data(trans(np.sin(theta[i])*np.cos(angle1), np.sin(theta[i])*np.sin(angle1), np.cos(theta[i]))[0], trans(np.sin(theta[i])*np.cos(angle1), np.sin(theta[i])*np.sin(angle1), np.cos(theta[i]))[1])
+    line82.set_data(trans(np.sin(theta[i])*np.cos(angle2), np.sin(theta[i])*np.sin(angle2), np.cos(theta[i]))[0], trans(np.sin(theta[i])*np.cos(angle2), np.sin(theta[i])*np.sin(angle2), np.cos(theta[i]))[1])
+    return line1, line2, line3, line4, line5, line6, line7, line62, line72, line8, line82
 
-# call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=20000, interval=0.001, blit=True)
 
-# save the animation as an mp4.  This requires ffmpeg or mencoder to be
-# installed.  The extra_args ensure that the x264 codec is used, so that
-# the video can be embedded in html5.  You may need to adjust this for
-# your system: for more information, see
-# http://matplotlib.sourceforge.net/api/animation_api.html
-
-
-plt.show()
-'''
